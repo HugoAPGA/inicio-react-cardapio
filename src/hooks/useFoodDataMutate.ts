5ad85b1cac49b72c81c55fe550b7d9eaef7d4394
HugoAPGA <hugoaga2006@gmail.com>
2025-11-09 16:18:12 -0300

    Add files via upload

---
+++ src/hooks/useFoodDataMutate.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { type AxiosPromise } from "axios";
+import type { FoodData } from "../interface/FoodData";
+
+const API_URL = "http://localhost:8080";
+
+const postData = async (data: FoodData): AxiosPromise<unknown> => {
+  const response = axios.post(API_URL + "/food", data);
+  return response;
+};
+
+export function useFoodDataMutate() {
+  const queryClient = useQueryClient();
+  const { mutate, isPending, isSuccess } = useMutation({
+    mutationFn: postData,
+    retry: 2,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["food-data"] });
+    },
+  });
+
+  return {
+    mutate,
+    isLoading: isPending,
+    isSuccess,
+  };
+}
